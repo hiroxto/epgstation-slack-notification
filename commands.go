46849f8ff20968147106c21aea403f46f2ec05d6
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-03-09 16:52:37 +0900

    Create startPreCommandNotification and startRecCommandNotification function

--- commands.go
+++ commands.go
@@ -15,6 +15,44 @@ var commands = []*cli.Command{
 	commandRecordedFailed,
 }
 
+func startPreCommandNotification(context *cli.Context, env PreCommandEnv, config Config, commandConfig CommandConfigStruct) error {
+	if !commandConfig.Enable {
+		displayCommandIsDisableMessage(context)
+		return nil
+	}
+
+	slackAPIKey := config.Slack.APIKey
+	slackChannel := getSlackChannel(config.Slack.Channel, commandConfig.Channel)
+	slackClient := createSlackClient(slackAPIKey, context.Bool("debug"))
+	blocks := buildPreCommandBlocks(commandConfig.Message, env)
+	_, _, err := slackClient.PostMessage(slackChannel, blocks)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func startRecCommandNotification(context *cli.Context, env RecCommandEnv, config Config, commandConfig CommandConfigStruct) error {
+	if !commandConfig.Enable {
+		displayCommandIsDisableMessage(context)
+		return nil
+	}
+
+	slackAPIKey := config.Slack.APIKey
+	slackChannel := getSlackChannel(config.Slack.Channel, commandConfig.Channel)
+	slackClient := createSlackClient(slackAPIKey, context.Bool("debug"))
+	blocks := buildRecCommandBlocks(commandConfig.Message, env)
+	_, _, err := slackClient.PostMessage(slackChannel, blocks)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func displayCommandIsDisableMessage(context *cli.Context) {
 	fmt.Printf("%s command is disabled.\n", context.Command.Name)
 }
