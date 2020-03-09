f913cb7b4aaac788211fb1106bba3845c9cbd678
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-03-09 16:54:46 +0900

    Use startPreCommandNotification() in recorded-pre-start command

--- command_recorded_pre_start.go
+++ command_recorded_pre_start.go
@@ -18,18 +18,7 @@ var commandRecordedPreStart = &cli.Command{
 func commandRecordedPreStartAction(context *cli.Context) error {
 	config := loadConfigFile()
 	env := loadPreCommandEnvs()
-	commandConfig := config.Commands.RecordedPreStart
-
-	if !commandConfig.Enable {
-		displayCommandIsDisableMessage(context)
-		return nil
-	}
-
-	slackAPIKey := config.Slack.APIKey
-	slackChannel := getSlackChannel(config.Slack.Channel, commandConfig.Channel)
-	slackClient := createSlackClient(slackAPIKey, context.Bool("debug"))
-	blocks := buildPreCommandBlocks(commandConfig.Message, env)
-	_, _, err := slackClient.PostMessage(slackChannel, blocks)
+	err := startPreCommandNotification(context, env, config, config.Commands.RecordedPreStart)
 
 	if err != nil {
 		log.Fatal(err.Error())
