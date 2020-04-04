7925bbf5aaa38c320bcde5b815667efa51fc6ad4
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-04-05 04:54:10 +0900

    CommandEnv struct を作成

--- env.go
+++ env.go
@@ -37,6 +37,23 @@ type RecCommandEnv struct {
 	LogPath     string `envconfig:"LOGPATH" default:"None"`
 }
 
+// CommandEnv コマンドに渡される変数
+type CommandEnv struct {
+	RecordedID  string `envconfig:"RECORDEDID" default:"None"`
+	ProgramID   string `envconfig:"PROGRAMID" default:"None"`
+	ChannelType string `envconfig:"CHANNELTYPE" default:"None"`
+	ChannelID   string `envconfig:"CHANNELID" default:"None"`
+	ChannelName string `envconfig:"CHANNELNAME" default:"None"`
+	StartAt     string `envconfig:"STARTAT" default:"None"`
+	EndAt       string `envconfig:"ENDAT" default:"None"`
+	Duration    string `envconfig:"DURATION" default:"None"`
+	Name        string `envconfig:"NAME" default:"None"`
+	Description string `envconfig:"DESCRIPTION" default:"None"`
+	Extended    string `envconfig:"EXTENDED" default:"None"`
+	RecPath     string `envconfig:"RECPATH" default:"None"`
+	LogPath     string `envconfig:"LOGPATH" default:"None"`
+}
+
 func loadPreCommandEnv() (PreCommandEnv, error) {
 	var env PreCommandEnv
 
