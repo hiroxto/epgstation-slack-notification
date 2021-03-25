477fce515b74e41c1d1b9c0ad0c06787b552fb7e
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2021-03-25 22:05:10 +0900

    環境変数 HALF_WIDTH_CHANNELNAME を HalfWidthChannelName として追加

--- env.go
+++ env.go
@@ -8,19 +8,20 @@ import (
 
 // CommandEnv コマンドに渡される変数
 type CommandEnv struct {
-	RecordedID  string `envconfig:"RECORDEDID" default:"None"`
-	ProgramID   string `envconfig:"PROGRAMID" default:"None"`
-	ChannelType string `envconfig:"CHANNELTYPE" default:"None"`
-	ChannelID   string `envconfig:"CHANNELID" default:"None"`
-	ChannelName string `envconfig:"CHANNELNAME" default:"None"`
-	StartAt     string `envconfig:"STARTAT" default:"None"`
-	EndAt       string `envconfig:"ENDAT" default:"None"`
-	Duration    string `envconfig:"DURATION" default:"None"`
-	Name        string `envconfig:"NAME" default:"None"`
-	Description string `envconfig:"DESCRIPTION" default:"None"`
-	Extended    string `envconfig:"EXTENDED" default:"None"`
-	RecPath     string `envconfig:"RECPATH" default:"None"`
-	LogPath     string `envconfig:"LOGPATH" default:"None"`
+	RecordedID           string `envconfig:"RECORDEDID" default:"None"`
+	ProgramID            string `envconfig:"PROGRAMID" default:"None"`
+	ChannelType          string `envconfig:"CHANNELTYPE" default:"None"`
+	ChannelID            string `envconfig:"CHANNELID" default:"None"`
+	ChannelName          string `envconfig:"CHANNELNAME" default:"None"`
+	HalfWidthChannelName string `envconfig:"HALF_WIDTH_CHANNELNAME" default:"None"`
+	StartAt              string `envconfig:"STARTAT" default:"None"`
+	EndAt                string `envconfig:"ENDAT" default:"None"`
+	Duration             string `envconfig:"DURATION" default:"None"`
+	Name                 string `envconfig:"NAME" default:"None"`
+	Description          string `envconfig:"DESCRIPTION" default:"None"`
+	Extended             string `envconfig:"EXTENDED" default:"None"`
+	RecPath              string `envconfig:"RECPATH" default:"None"`
+	LogPath              string `envconfig:"LOGPATH" default:"None"`
 }
 
 func loadCommandEnv() (CommandEnv, error) {
