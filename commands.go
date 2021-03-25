47f6125c3f1721c480e15999fe58db9f529f9c50
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2021-03-25 20:51:01 +0900

    recorded-pre-startをrecording-pre-startに変更
    古いコマンド名であるrecorded-pre-startはエイリアスで使用可能

--- commands.go
+++ commands.go
@@ -7,7 +7,7 @@ import (
 
 var commands = []*cli.Command{
 	commandReserveNewAddition,
-	commandRecordedPreStart,
+	commandRecordingPreStart,
 	commandRecordedPrepRecFailed,
 
 	commandRecordedStart,
