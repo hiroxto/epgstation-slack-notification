2192ffaa5823ae40710869202fca1f4157bc168a
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2021-03-25 21:31:21 +0900

    reserveDeletedCommandで使う設定を追加

--- config.go
+++ config.go
@@ -19,6 +19,7 @@ type Config struct {
 	Commands struct {
 		ReserveNewAddition     CommandConfig `yaml:"reserve-new-addition"`
 		ReserveUpdateCommand   CommandConfig `yaml:"reserve-update"`
+		ReserveDeletedCommand  CommandEnv    `yaml:"reserve-deleted"`
 		RecordingPreStart      CommandConfig `yaml:"recording-pre-start"`
 		RecordingPrepRecFailed CommandConfig `yaml:"recording-prep-rec-failed"`
 		RecordingStart         CommandConfig `yaml:"recording-start"`
