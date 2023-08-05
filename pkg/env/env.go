1180f94d8a6e19ff18539385e6cfffac41c1f690
Hiroto Kitazawa <hiroxto@gmail.com>
2023-08-06 01:26:45 +0900

    feat: 予約コマンドと録画コマンドの環境変数を分離

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -4,6 +4,24 @@ import "github.com/kelseyhightower/envconfig"
 
 // See: https://github.com/l3tnun/EPGStation/blob/master/doc/conf-manual.md
 
+// ReserveCommandEnv 予約コマンドと録画準備コマンドに渡される環境変数
+type ReserveCommandEnv struct {
+	ProgramID            string `envconfig:"PROGRAMID" default:"None"`
+	ChannelType          string `envconfig:"CHANNELTYPE" default:"None"`
+	ChannelID            string `envconfig:"CHANNELID" default:"None"`
+	ChannelName          string `envconfig:"CHANNELNAME" default:"None"`
+	HalfWidthChannelName string `envconfig:"HALF_WIDTH_CHANNELNAME" default:"None"`
+	StartAt              string `envconfig:"STARTAT" default:"None"`
+	EndAt                string `envconfig:"ENDAT" default:"None"`
+	Duration             string `envconfig:"DURATION" default:"None"`
+	Name                 string `envconfig:"NAME" default:"None"`
+	HalfWidthName        string `envconfig:"HALF_WIDTH_NAME" default:"None"`
+	Description          string `envconfig:"DESCRIPTION" default:"None"`
+	HalfWidthDescription string `envconfig:"HALF_WIDTH_DESCRIPTION" default:"None"`
+	Extended             string `envconfig:"EXTENDED" default:"None"`
+	HalfWidthExtended    string `envconfig:"HALF_WIDTH_EXTENDED" default:"None"`
+}
+
 // RecordingCommandEnv コマンドに渡される変数
 type RecordingCommandEnv struct {
 	RecordedID           string `envconfig:"RECORDEDID" default:"None"`
