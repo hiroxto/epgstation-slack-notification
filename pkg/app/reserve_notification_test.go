452f03fdb73b354a7f30130d5bceb657893e41fa
Hiroto Kitazawa <hiroxto@gmail.com>
2024-05-11 21:40:07 +0900

    feat: 予約情報の構造体を作成

---
+++ pkg/app/reserve_notification_test.go
@@ -0,0 +1,54 @@
+package app
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/hiroxto/epgstation-slack-notification/pkg/env"
+)
+
+func Test_ReserveDetailFromEnv(t *testing.T) {
+	env := env.ReserveCommandEnv{
+		ProgramID:            "PROGRAMID",
+		ChannelType:          "CHANNELTYPE",
+		ChannelID:            "CHANNELID",
+		ChannelName:          "CHANNELNAME",
+		HalfWidthChannelName: "HALF_WIDTH_CHANNELNAME",
+		StartAt:              "1715353200000", // Unix Millis = 2024-05-11T00:00:00+09:00
+		EndAt:                "1715355000000", // Unix Millis = 2024-05-11T00:30:00+09:00
+		Duration:             "1800000",       // ms = 30 min
+		Name:                 "NAME",
+		HalfWidthName:        "HALF_WIDTH_NAME",
+		Description:          "DESCRIPTION",
+		HalfWidthDescription: "HALF_WIDTH_DESCRIPTION",
+		Extended:             "EXTENDED",
+		HalfWidthExtended:    "HALF_WIDTH_EXTENDED",
+	}
+
+	actual := ReserveDetailFromEnv(env)
+
+	expected := ReserveDetail{
+		ProgramID:            "PROGRAMID",
+		ChannelType:          "CHANNELTYPE",
+		ChannelID:            "CHANNELID",
+		ChannelName:          "CHANNELNAME",
+		HalfWidthChannelName: "HALF_WIDTH_CHANNELNAME",
+		StartAt:              "1715353200000",
+		EndAt:                "1715355000000",
+		Duration:             uint(1800000),
+		Name:                 "NAME",
+		HalfWidthName:        "HALF_WIDTH_NAME",
+		Description:          "DESCRIPTION",
+		HalfWidthDescription: "HALF_WIDTH_DESCRIPTION",
+		Extended:             "EXTENDED",
+		HalfWidthExtended:    "HALF_WIDTH_EXTENDED",
+		StartAtTime:          time.UnixMilli(1715353200000),
+		EndAtTime:            time.UnixMilli(1715355000000),
+		DurationMin:          30,
+	}
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("expected = %v, actual = %v", expected, actual)
+	}
+}
