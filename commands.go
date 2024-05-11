bb437c0d54f04a48d018f8f93dafc9cfaf345ad3
Hiroto Kitazawa <hiroxto@gmail.com>
2024-05-11 23:45:07 +0900

    chore: 不要になったコードを削除

--- commands.go
+++ commands.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/hiroxto/epgstation-slack-notification/pkg/command"
 	"github.com/urfave/cli/v2"
 )
@@ -21,67 +19,4 @@ var commands = []*cli.Command{
 	command.EncodingFinishCommand,
 
 	command.DumpEnvsCommand,
-}
-
-func startCommandNotification(context *cli.Context, env interface{}, config Config, commandConfig CommandConfig) error {
-	if !commandConfig.Enable {
-		fmt.Printf("%s command is disabled.\n", context.Command.Name)
-		return nil
-	}
-
-	slackAPIKey := config.Slack.APIKey
-	slackChannel := config.Slack.Channel
-	if len(commandConfig.Channel) > 0 {
-		slackChannel = commandConfig.Channel
-	}
-
-	slackClient, err := createSlackClient(slackAPIKey, context.Bool("debug"))
-	if err != nil {
-		return err
-	}
-
-	message, err := formatCommandEnv("", commandConfig.Message, env)
-
-	if err != nil {
-		return err
-	}
-
-	fields, err := buildCommandFields(commandConfig.FieldsSection, env)
-
-	if err != nil {
-		return err
-	}
-
-	options := buildMessageOptions(message, fields)
-	_, _, err = slackClient.PostMessage(slackChannel, options)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func startRecordedLogNotification(context *cli.Context, env RecordingCommandEnv, config Config, commandConfig CommandConfig) error {
-	recordedLog, err := getRecordedLog(env.RecordedID, config)
-	if err != nil {
-		return err
-	}
-	slackAPIKey := config.Slack.APIKey
-	slackChannel := config.Slack.Channel
-	if len(commandConfig.Channel) > 0 {
-		slackChannel = commandConfig.Channel
-	}
-	slackClient, err := createSlackClient(slackAPIKey, context.Bool("debug"))
-	if err != nil {
-		return err
-	}
-	fields, err := buildRecordedLogReportFields(recordedLog)
-	if err != nil {
-		return err
-	}
-	options := buildMessageOptions("番組(ID:"+env.RecordedID+")のエラー/ドロップ/スクランブル数レポート", fields)
-	_, _, err = slackClient.PostMessage(slackChannel, options)
-
-	return nil
 }
