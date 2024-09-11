e7f14cd420126fb9b3b52f1a427a1cddc1fec236
Hiroto Kitazawa <hiroxto@gmail.com>
2024-09-12 00:24:07 +0900

    chore: デバッグ環境が変わったのでビルドスクリプトも微修正

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ docker run --rm -it -v "$PWD":/_ -w /_ golang:1-alpine sh -c '
     dirname="dist"
     filename="epgstation-slack-notification"
 
-    (GOOS=darwin GOARCH=arm64 go build -o "${dirname}/darwin/${filename}") &
-    (GOOS=linux GOARCH=arm GOARM=7 go build -o "${dirname}/linux-arm-7/${filename}") &
+    (GOOS=darwin GOARCH=arm64 go build -o "${dirname}/darwin-arm64/${filename}") &
+    (GOOS=linux GOARCH=amd64 go build -o "${dirname}/linux-amd64/${filename}") &
     wait
 '
