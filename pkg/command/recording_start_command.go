459ead0b4099e5ee5f866f058d63dc186861d0a3
Hiroto Kitazawa <hiroxto@gmail.com>
2024-09-16 09:29:15 +0900

    refactor: パッケージ名と被る変数名をリネーム

--- pkg/command/recording_start_command.go
+++ pkg/command/recording_start_command.go
@@ -27,18 +27,18 @@ func recordingStartCommandAction(context *cli.Context) error {
 		return err
 	}
 
-	config, err := config.LoadConfigFromYaml([]byte(configData))
+	conf, err := config.LoadConfigFromYaml([]byte(configData))
 	if err != nil {
 		return err
 	}
 
-	commandConfig := config.Commands.RecordingStart
+	commandConfig := conf.Commands.RecordingStart
 	if !commandConfig.Enable {
 		fmt.Printf("%s command is disabled.\n", context.Command.Name)
 		return nil
 	}
-	slackAPIKey := config.Slack.APIKey
-	slackChannel := config.Slack.Channel
+	slackAPIKey := conf.Slack.APIKey
+	slackChannel := conf.Slack.Channel
 	if len(commandConfig.Channel) > 0 {
 		slackChannel = commandConfig.Channel
 	}
