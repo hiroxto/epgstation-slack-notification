b55ad52c328f92f971676916ab44008a597f136c
Hiroto Kitazawa <hiroxto@gmail.com>
2024-09-17 22:44:17 +0900

    fix: Slackクライアントを作成する際にAuthTestをしないように修正

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -35,10 +35,6 @@ func FieldsFromConfig(fieldConfigs []config.FieldConfig) []Field {
 func createSlackClient(apiKey string, debug bool) (*slack.Client, error) {
 	api := slack.New(apiKey, slack.OptionDebug(debug), slack.OptionLog(log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)))
 
-	if _, err := api.AuthTest(); err != nil {
-		return nil, err
-	}
-
 	return api, nil
 }
 
